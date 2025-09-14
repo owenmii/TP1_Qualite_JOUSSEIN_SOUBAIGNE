e73ec4aaa457a1e7f637c43a2c7da736831b1181
owenmii <owen.soubaigne@gmail.com>
2025-09-14 18:30:12 +0200

    Ajout classe Add et méthode add avec test unitaire

--- src/test/java/testAdd.java
+++ src/test/java/testAdd.java
@@ -1,3 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class testAdd {
-    
+    @Test
+    public void testAddition() {
+        assertEquals(5, Add.add(2, 3));
+        assertEquals(-1, Add.add(2, -3));
+        assertEquals(0, Add.add(0, 0));
+        assertEquals(-5, Add.add(-2, -3));
+    }
 }
