b1871d00ea9a94073eb8089d0a58457c473be08b
owenmii <owen.soubaigne@gmail.com>
2025-09-14 18:56:21 +0200

    première ligne de test modifiée

--- src/test/java/testAdd.java
+++ src/test/java/testAdd.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class testAdd {
     @Test
     public void testAddition() {
-        assertEquals(5, add.add(2, 3));
+        assertEquals(8, add.add(5, 3));
         assertEquals(-1, add.add(2, -3));
         assertEquals(0, add.add(0, 0));
         assertEquals(-5, add.add(-2, -3));
